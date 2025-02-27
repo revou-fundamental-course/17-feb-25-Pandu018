63ec7efb19ba0351302e88afcd5c6f520ab3bb03
Pandu018 <studyrap612@gmail.com>
2025-02-27 23:55:43 +0700

    MINI PROJECT REVOU

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,19 @@
 // ini untuk javascript //
+console.log('succes Load external JS')
+
+
+function validateForm() {
+    const name = document.forms['message-form']['name'].value;
+    const email = document.forms['message-form']['email'].value;
+    const message = document.forms['message-form']['message'].value;
+    
+    
+    // validasi nama
+    if (name == ''|| message == '' || email == '') {
+        alert('Tidak boleh ada yang kosong')
+        return false;
+    } else {
+        alert('Terima kasih Bapak/Ibu' + name);
+}
+}
+
